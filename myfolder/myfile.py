dec0471859c908c0aa511f8312717b009eeaffc3
iraklisartania09-web <iraklisartania09@gmail.com>
2026-06-29 14:15:25 +0100

    Add files via upload

---
+++ myfolder/myfile.py
@@ -0,0 +1,11 @@
+#print("""Welcome to Future Builders Camp!
+#Today I wrote my firs python program.
+#Pyhon is my first step into future technology""")
+
+#age = 16
+#name = "Irakli"
+#technology = "Artificial Intelligence"
+#print(age ," ", name ," ", technology)
+
+name = input("What's your name? ")
+print("Hello", name + ",","welcome to the world of coding!")
